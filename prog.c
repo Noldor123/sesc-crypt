3a13c01b345e696c0eb516964834fa65f0176f20
Noldor123 <37693340+Noldor123@users.noreply.github.com>
2018-11-30 22:54:42 +0700

    Update prog.c

--- prog.c
+++ prog.c
@@ -1,15 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <crypt.h>
 
 int main(void) {
   char *str = NULL, *hash = NULL;
-  scanf("%s", str);
-  scanf("%s", hash);
+  size_t lstr = 0, lhash = 0;
+  getline(&str, &lstr, stdin);
+  getline(&hash, &lhash, stdin);
   if (str == NULL || hash == NULL) {
     printf("error\n");
-  }else{
+    return 0;
+  }
+  while(str[strlen(str) - 1] == '\n' || str[strlen(str) - 1] == '\r') str[strlen(str) - 1] = '\0';
+  while(hash[strlen(hash) - 1] == '\n' || hash[strlen(hash) - 1] == '\r') hash[strlen(hash) - 1] = '\0';
   char *realHash = crypt(str, "00");
   if (realHash == NULL)
     printf("error\n");
@@ -17,6 +22,5 @@ int main(void) {
     printf("allow\n");
   else
     printf("deny\n");
-  }
   return 0;
 }
