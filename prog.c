361ca5fbd853fe483884a5fb99cf4437c98b0711
Noldor123 <37693340+Noldor123@users.noreply.github.com>
2018-11-30 22:03:59 +0700

    Create prog.c

---
+++ prog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <crypt.h>
+
+int main(void) {
+  char *str = NULL, *hash = NULL;
+  scanf("%s", str);
+  scanf("%s", hash);
+  if (str == NULL || hash == NULL) {
+    printf("error\n");
+  }else{
+  char *realHash = crypt(str, "00");
+  if (realHash == NULL)
+    printf("error\n");
+  else if (strcmp(hash, realHash) == 0)
+    printf("allow\n");
+  else
+    printf("deny\n");
+  }
+  return 0;
+}
